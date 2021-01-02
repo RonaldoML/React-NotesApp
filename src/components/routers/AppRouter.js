336c69b9e1eb1c19d37fd261f75abc6c23c32196
RonaldoML <rmonserr@espol.edu.ec>
2021-01-02 17:06:24 -0500

    Mantener estado de autenticación, protección de rutas privadas y públicas

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { login } from '../../actions/auth';
+import { firebase } from '../../firebase/firebase-config';
 import { JournalScreen } from '../journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    //Para mantener el estado del login
+    const [checking, setChecking] = useState(true);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    //Se lanza cada vez que se cambia el usuario autenticado
+    useEffect(() => {
+
+        firebase.auth().onAuthStateChanged((user) => {
+
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+                setIsLoggedIn( true );
+            }else{
+                setIsLoggedIn(false);
+            }
+
+            setChecking(false);
+        })
+
+    }, [dispatch, setChecking, setIsLoggedIn]);
+//se muestra una pantalla de espera mientras se autentica en firebase
+    if( checking ){
+        return (
+            <h1>Espere...</h1>
+        )
+    }
+
     return (
 
         <Router>
             <div>
                 <Switch>
-                    <Route path="/auth" component={AuthRouter}>
-                    </Route>
-                    <Route exact path="/" component={JournalScreen}>
-                    </Route>
-                    <Redirect to="/auth/login" />
+                    <PublicRoute isAuthenticated={isLoggedIn} path="/auth/login" component={AuthRouter} exact>
+                    </PublicRoute>
+                    <PrivateRoute isAuthenticated={isLoggedIn} path="/" component={JournalScreen}>
+                    </PrivateRoute>
                 </Switch>
             </div>
         </Router>
